d77d89d467b6c80f9fa8527a1950b3fabbf95e8d
devimukepshene <74088089+devimukepshene@users.noreply.github.com>
2022-03-24 09:32:34 +0530

    Add files via upload

---
+++ day 12 task.cs
@@ -0,0 +1,48 @@
+<html>
+<head>
+<title>
+application form
+</title>
+</head>
+<body bgcolor="pink">
+<form action="regsuccess.html">
+<h1><marquee style="color:neon"scrollamount="20" width="2000" height="150">WELCOME TO SOFTURA!!!</marquee></h1>
+<h1><center><marquee style="color:purple" direction="up">Freshers required for software engineer-trainee!!!</center></marquee></h1><br/>
+<h2 color="white"><b><center>Application Form</center></b></h2><br/>
+<form>
+<table>
+<label><b>First Name:</label>
+<input type="text" name="fname"/>
+<br/>
+<label><b>Last Name:</b></label>
+<input type="text" name="lname"/><br/>
+<label><b>Date Of Birth:</label>
+<input type="text" name="dob"/><br/>
+<label><b>Board Of education</b></label>
+<input type="radio" name="boe" value="Cbse"/><label>Cbse</label>
+<input type="radio" name="boe" value="State Board"/><label>State Board</label><br/>
+<label> Percentage scored in X :</label>
+<input type="text" name="pscore"/><br/>
+<label> Percentage scored in XII :</label>
+<input type="text" name="pscore12"/><br/>
+<label>Name of the College :</label>
+<input type="text" name="noc"/><br/>
+<label> degree :</label>
+<input type="checkbox" name="qual" value="BE"><label> BE</label>
+<input type="checkbox" name="qual" value="BTech"><label> B.Tech</label>
+<input type="checkbox" name="qual" value="B.Sc"><label> B.Sc</label>
+<input type="checkbox" name="qual" value="M.Sc"><label> M.Sc</label>
+<input type="checkbox" name="qual" value="M.Tech"><label> M.Tech</label><br/>
+<label> Percentage scored in degree:</label>
+<input type="text" name="psd"/><br/>
+<label> Contact Number:</label>
+<input type="text" name="cnum"/><br/>
+<label> Address :</label>
+<input type="text" name="add"/><br/>
+<a href="C:\nikshi\terms&conditions.html"><input type="radio"name="tercon" />I agree to the Terms & Conditions</a><br/>
+<input type="submit" value="register">
+
+
+</form>
+</body>
+</html>
